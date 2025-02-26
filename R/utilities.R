e6989c6a75951644fd33eb70f3946b5b6660448e
viv3ckj <viveck.kingsley@gmail.com>
2025-02-26 15:01:45 +0000

    Added tilde uppercase A + copyright symbol to dict and tests, and removed excess tests

--- R/utilities.R
+++ R/utilities.R
@@ -31,23 +31,32 @@ fix_encoding <- function(string) {
   # the data cleaning problem.
 
   # This includes sequences starting with:
-  # - Latin Capital letter
-  # - Latin Small Letter A with circumflex
+  # - Latin Capital letter A with tilde (U+00C3)
+  # - Latin Small Letter A with circumflex (U+00E2)
   replacement_dict <- c(
-    # All these still need to be replaces with unicode
-
     # Although some numbers here are technically subscript
     # we are using the normal format to match the most used formatting.
     # We might also want to replace subscript numbers to regular text
     # e.g., 1 instead of subscript 1, but for now we wont address this and
     # accept the differences in the raw data.
+    
+    # The following mapping is used below:
+    # \u00E2 = Latin Small Letter A with circumflex
+    # \u002C = Comma
+    # \u0081 = High Octet Preset
+    # \u20AC = Euro sign
+    # \u2122 = Trade Mark Sign
+    # \u2020 = Dagger
+    # \u00B6 = Pilcrow Sign
+    # \u00A9 = Copyright Symbol
     "\u00E2\u002C\u0081" = "1",
     "\u00E2\u20AC\u2122" = "'",
     "\u00E2\u20AC\u201C" = "-",
     "\u00E2\u002C\u2020" = "6",
     "\u00E2\u002C\u002C" = "2",
     
-    "\u00C3\u00B6" = "\u00F6"
+    "\u00C3\u00B6" = "\u00F6",
+    "\u00C3\u00A9" = "\u00E9"
   )
 
   str_replace_all(string, replacement_dict)
