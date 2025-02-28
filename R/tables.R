a4cc07864bb4c8d135fc081fbdcd2b87bb803d82
Milan Wiedemann <milan.wiedemann@gmail.com>
2025-02-28 10:56:56 +0000

    Improve download buttons

--- R/tables.R
+++ R/tables.R
@@ -1,5 +1,5 @@
 #' Helper function to create code usage table
-#' @importFrom DT datatable formatPercentage
+#' @importFrom DT datatable formatPercentage formatStyle styleEqual
 #' @keywords internal
 datatable_usage <- function(data) {
   datatable(data,
@@ -32,7 +32,7 @@ datatable_codelist <- function(data, data_desc) {
     data,
     colnames = c("Code", "Description", "Usage data"),
     rownames = FALSE,
-    extensions = c("Buttons", "Scroller"),
+    extensions = c("Scroller"),
     options = list(
       columnDefs = list(
         list(width = "50px", targets = 0),
@@ -40,26 +40,18 @@ datatable_codelist <- function(data, data_desc) {
         list(width = "100px", targets = 2)
       ),
       order = list(list(2, "desc")),
-      pageLength = 20,
+      pageLength = 10,
       scrollX = TRUE,
       searching = FALSE,
-      dom = "Bfrtip",
-      buttons = list(
-        "copy",
-        list(
-          extend = "csv",
-          title = paste0("selected_", data_desc, "_codes")
-        )
-      ),
       deferRender = TRUE,
       scrollY = 400,
       scroller = TRUE
     )
-  ) |> 
-    DT::formatStyle(
-      "usage_data_available", 
-      color = DT::styleEqual(
-        c("Available", "Not available"), 
+  ) |>
+    formatStyle(
+      "usage_data_available",
+      color = styleEqual(
+        c("Available", "Not available"),
         c("#35B779FF", "#ED6925FF")
       ),
       fontWeight = "bold"
