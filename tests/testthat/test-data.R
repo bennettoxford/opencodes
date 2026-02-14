3b8e59d2d9561b5bc619eb353f40cd2a455d2a46
kayleepn <kayleepoon852@gmail.com>
2026-02-14 12:47:20 +0000

    Add breakdowns for ICD-10 code usage

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -70,7 +70,7 @@ test_that("Test SNOMEDCT missing description", {
   expect_equal(test_sum_missing_description, 0)
 })
 
-# This tests if we read in the codes correctly. We might want to improve this to test more than one case in futre.
+# This tests if we read in the codes correctly. We might want to improve this to test more than one case in the future.
 # This illustrates the problem:
 # $ python -c 'print(int(float("39733011000001106")))'
 # 39733011000001104
@@ -136,6 +136,119 @@ test_that("Test no non-alphanumeric characters in ICD-10 codes", {
 test_that("Test ICD10 missing description", {
   test_sum_missing_description <- sum(is.na(icd10_usage$description))
   expect_equal(test_sum_missing_description, 0)
+})
+
+# ICD-10 Breakdowns Tests
+test_that("Test icd10_usage_breakdowns column names", {
+  test_names <- names(icd10_usage_breakdowns)
+  expect_equal(
+    test_names,
+    c(
+      "start_date",
+      "end_date",
+      "icd10_code",
+      "description",
+      "breakdown",
+      "usage"
+    )
+  )
+})
+
+test_that("Test icd10_usage_breakdowns column types", {
+  expect_s3_class(icd10_usage_breakdowns$start_date, "Date")
+  expect_s3_class(icd10_usage_breakdowns$end_date, "Date")
+  expect_type(icd10_usage_breakdowns$icd10_code, "character")
+  expect_type(icd10_usage_breakdowns$description, "character")
+  expect_type(icd10_usage_breakdowns$breakdown, "character")
+  expect_type(icd10_usage_breakdowns$usage, "integer")
+})
+
+test_that("Test icd10_usage_breakdowns date range", {
+  test_range_start_date <- range(icd10_usage_breakdowns$start_date)
+  test_range_end_date <- range(icd10_usage_breakdowns$end_date)
+
+  expect_equal(
+    test_range_start_date,
+    c(as.Date("2012-04-01"), as.Date("2024-04-01"))
+  )
+  expect_equal(
+    test_range_end_date,
+    c(as.Date("2013-03-31"), as.Date("2025-03-31"))
+  )
+})
+
+test_that("Test icd10_usage_breakdowns breakdown values", {
+  expected_breakdowns <- c(
+    "all_diagnoses",
+    "main_diagnosis",
+    "male",
+    "female",
+    "gender_unknown",
+    "age_0",
+    "age_1_4",
+    "age_5_9",
+    "age_10_14",
+    "age_15",
+    "age_16",
+    "age_17",
+    "age_18",
+    "age_19",
+    "age_20_24",
+    "age_25_29",
+    "age_30_34",
+    "age_35_39",
+    "age_40_44",
+    "age_45_49",
+    "age_50_54",
+    "age_55_59",
+    "age_60_64",
+    "age_65_69",
+    "age_70_74",
+    "age_75_79",
+    "age_80_84",
+    "age_85_89",
+    "age_90plus"
+  )
+  actual_breakdowns <- unique(icd10_usage_breakdowns$breakdown)
+  expect_setequal(actual_breakdowns, expected_breakdowns)
+})
+
+test_that("Test no non-alphanumeric characters in ICD-10 breakdown codes", {
+  non_alphanumeric_codes <- icd10_usage_breakdowns$icd10_code[grep(
+    "\\s?[^[:alnum:]]+\\s?",
+    icd10_usage_breakdowns$icd10_code
+  )]
+  expect_equal(length(non_alphanumeric_codes), 0)
+})
+
+test_that("Test ICD-10 breakdowns missing description", {
+  test_sum_missing_description <- sum(is.na(icd10_usage_breakdowns$description))
+  expect_equal(test_sum_missing_description, 0)
+})
+
+test_that("Test ICD-10 breakdowns totals match main dataset", {
+  # Get first 3 unique codes
+  test_codes <- unique(icd10_usage$icd10_code)[1:3]
+
+  for (code in test_codes) {
+    # Get usage from main dataset
+    main_usage <- icd10_usage |>
+      dplyr::filter(icd10_code == code) |>
+      dplyr::select(start_date, end_date, usage) |>
+      dplyr::arrange(start_date)
+
+    # Get all_diagnoses breakdown usage
+    breakdown_usage <- icd10_usage_breakdowns |>
+      dplyr::filter(icd10_code == code, breakdown == "all_diagnoses") |>
+      dplyr::select(start_date, end_date, usage) |>
+      dplyr::arrange(start_date)
+
+    expect_equal(
+      main_usage,
+      breakdown_usage,
+      info = paste("Mismatch for ICD-10 code:", code)
+    )
+  }
 })
 
 # OPCS-4 Tests
@@ -233,13 +346,35 @@ test_that("Test opcs4_usage_breakdowns date range", {
 
 test_that("Test opcs4_usage_breakdowns breakdown values", {
   expected_breakdowns <- c(
-    "all_procedures", "main_procedure",
-    "male", "female", "gender_unknown",
-    "age_0", "age_1_4", "age_5_9", "age_10_14",
-    "age_15", "age_16", "age_17", "age_18", "age_19",
-    "age_20_24", "age_25_29", "age_30_34", "age_35_39", "age_40_44",
-    "age_45_49", "age_50_54", "age_55_59", "age_60_64", "age_65_69",
-    "age_70_74", "age_75_79", "age_80_84", "age_85_89", "age_90plus"
+    "all_procedures",
+    "main_procedure",
+    "male",
+    "female",
+    "gender_unknown",
+    "age_0",
+    "age_1_4",
+    "age_5_9",
+    "age_10_14",
+    "age_15",
+    "age_16",
+    "age_17",
+    "age_18",
+    "age_19",
+    "age_20_24",
+    "age_25_29",
+    "age_30_34",
+    "age_35_39",
+    "age_40_44",
+    "age_45_49",
+    "age_50_54",
+    "age_55_59",
+    "age_60_64",
+    "age_65_69",
+    "age_70_74",
+    "age_75_79",
+    "age_80_84",
+    "age_85_89",
+    "age_90plus"
   )
   actual_breakdowns <- unique(opcs4_usage_breakdowns$breakdown)
   expect_setequal(actual_breakdowns, expected_breakdowns)
